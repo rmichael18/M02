b34f2aa11b846396c86cf299f162792f21acbec2
rmichael18 <113042699+rmichael18@users.noreply.github.com>
2023-11-05 12:17:21 -0600

    Update M02 Case Study.py

--- M02 Case Study.py
+++ M02 Case Study.py
@@ -3,14 +3,14 @@
 # Determines whether or not a student makes the honor roll or deans list based on entered GPA
 while True:
     last_name = input("Enter the student's last name or 'ZZZ' to quit: ")
-    
+    first_name = input("Enter the student's first name: ")
     if last_name == 'ZZZ':
         break
     
     gpa = float(input("Enter the student's GPA: "))
     
     if gpa >= 3.5:
-        print(last_name, "has made the Dean's List.")
+        print(first_name, last_name, "has made the Dean's List.")
     
     elif gpa >= 3.25:
-        print(last_name, "has made the Honor Roll.")
+        print(first_name, last_name, "has made the Honor Roll.")
